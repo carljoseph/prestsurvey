b03a4214e05314cc35f3b637e909e137b0401465
Carl Joseph <carl@carljoseph.com.au>
2024-04-22 10:42:37 +1000

    stop tracking config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const LOCAL_API_KEY = '';
-const DEPLOYED_API_KEY = 'AIzaSyBCM2ItkeZw0o6oOCvpQ5neK2O4eAW1Btk';
-
-const Config = {
-    apiKey: window.location.hostname === 'localhost' ? LOCAL_API_KEY : DEPLOYED_API_KEY
-};
-
-export default Config;
