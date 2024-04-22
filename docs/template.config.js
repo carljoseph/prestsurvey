c318c353892c1710a09af94e9e51f5aec30780dd
Carl Joseph <carl@carljoseph.com.au>
2024-04-22 14:11:44 +1000

    add templated config.js

---
+++ docs/template.config.js
@@ -0,0 +1,7 @@
+// Add your PUBLIC_API_KEY for Google Maps JavaScript API
+// Save this file as config.js
+
+const Config = {
+    apiKey: '', // ADD YOUR PUBLIC_API_KEY
+  };
+  export default Config;
