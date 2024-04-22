e9a1e65a70845017b21b8eebad9468c11db062ee
Carl Joseph <carl@carljoseph.com.au>
2024-04-22 18:13:32 +1000

    tidy up js

--- docs/map.js
+++ docs/map.js
@@ -1,16 +1,15 @@
 import Config from './config.js';
 
-function loadGoogleMapsApi() {
+let markers = [];
+let selectedMarker = null;
+
+function loadGoogleMapsApi(callback) {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${Config.apiKey}&callback=initMap`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${Config.apiKey}&callback=${callback}`;
     script.async = true;
     script.defer = true;
     document.head.appendChild(script);
 }
-loadGoogleMapsApi();
-
-let markers = [];
-let selectedMarker = null;
 
 function createMarkerIcon(color) {
     return {
@@ -72,18 +71,15 @@ function filterMarkersByMonth() {
 }
 
 function initMap() {
-    var startLatLng = { lat: -37.7430, lng: 144.9665 };
-    var mapOptions = {
-        zoom: 14,
-        center: startLatLng
-    };
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    const startLatLng = { lat: -37.7430, lng: 144.9665 };
+    const mapOptions = { zoom: 14, center: startLatLng };
+    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     fetch('data/validated_addresses.json')
         .then(response => response.json())
         .then(locations => {
-            locations.forEach(function(location) {
-                let marker = new google.maps.Marker({
+            markers = locations.map(location => {
+                const marker = new google.maps.Marker({
                     position: new google.maps.LatLng(location.geocode_latitude, location.geocode_longitude),
                     map: map,
                     title: location.inferred_address,
@@ -91,81 +87,87 @@ function initMap() {
                     visible: true
                 });
 
-                // Set properties from the JSON data on the marker object
                 marker.set('inferred_interview_date', location.inferred_interview_date);
-                marker.set('image_filename', location.filename); // Ensure this matches the JSON property name
+                marker.set('image_filename', location.filename);
                 marker.set('archive_identifier', location.archive_identifier);
                 marker.set('archive_page_no', location.archive_page_no);
                 marker.set('interviewer', location.interviewer);
 
                 marker.addListener('click', function() {
-                    if (selectedMarker) {
-                        selectedMarker.setIcon(createMarkerIcon('blue'));
-                    }
-                    selectedMarker = marker;
-                    marker.setIcon(createMarkerIcon('red'));
-
-                    // Pass the properties directly to the showLocationInfo function
-                    showLocationInfo(
-                        marker.get('title'),
-                        marker.get('inferred_interview_date'),
-                        marker.get('archive_identifier'),
-                        marker.get('archive_page_no'),
-                        marker.get('interviewer')
-                    );
-                    showImageForMarker(marker.get('image_filename')); // Use the image_filename from the marker
-
-                    const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?id=${encodeURIComponent(marker.get('archive_identifier') + '-' + marker.get('archive_page_no'))}`;
-                    window.history.pushState({path: newUrl}, '', newUrl);
+                    updateSelectedMarker(marker);
+                    showLocationInfo(marker);
+                    showImageForMarker(marker.get('image_filename'));
+                    updateUrl(marker);
                 });
 
-                markers.push(marker);
+                return marker;
             });
 
-            // Check URL for query parameter and trigger click event on corresponding marker
-            const urlParams = new URLSearchParams(window.location.search);
-            const markerId = urlParams.get('id');
-            if (markerId) {
-                markers.forEach(marker => {
-                    const uniqueId = marker.get('archive_identifier') + '-' + marker.get('archive_page_no');
-                    if (uniqueId === markerId) {
-                        google.maps.event.trigger(marker, 'click');
-                    }
-                });
-            }
+            checkUrlForMarker();
         })
         .catch(error => console.error('Error fetching JSON:', error));
 }
 
-function showLocationInfo(address, date, archive_identifier, archive_page_no, interviewer) {
-    var content = '<h3>Inferred information</h3>' +
-                  '<p>' + address + '</p>' +
-                  '<p>Interviewed by ' + (interviewer || 'Unknown') +
-                  ' on ' + (date || 'Unknown') + '</p>' +
-                  '<p><b>Archive identifier:</b><br>' + archive_identifier + ' p. ' + archive_page_no + '</p>';
+function showLocationInfo(marker) {
+    const title = marker.get('title');
+    const inferred_interview_date = marker.get('inferred_interview_date');
+    const archive_identifier = marker.get('archive_identifier');
+    const archive_page_no = marker.get('archive_page_no');
+    const interviewer = marker.get('interviewer');
 
+    const content = `
+        <h3>Inferred information</h3>
+        <p>${title}</p>
+        <p>Interviewed by ${interviewer || 'Unknown'} on ${inferred_interview_date || 'Unknown'}</p>
+        <p><b>Archive identifier:</b><br>${archive_identifier} page ${archive_page_no}</p>
+    `;
     document.getElementById('image-info').innerHTML = content;
 }
 
 function showImageForMarker(imageFilename) {
-    var imageElement = document.getElementById('selected-image');
-    var imagePlaceholder = document.getElementById('image-placeholder');
+    const imageElement = document.getElementById('selected-image');
+    const imagePlaceholder = document.getElementById('image-placeholder');
+    imageElement.style.display = imageFilename ? 'block' : 'none';
+    imagePlaceholder.style.display = imageFilename ? 'none' : 'block';
+    imageElement.src = imageFilename ? `images/${imageFilename}` : '';
+}
 
-    if (imageFilename) {
-        imageElement.src = 'images/' + imageFilename;
-        imageElement.style.display = 'block';
-        imagePlaceholder.style.display = 'none';
-    } else {
-        imageElement.style.display = 'none';
-        imagePlaceholder.style.display = 'block';
+function updateUrl(marker) {
+    const uniqueId = `${marker.get('archive_identifier')}-${marker.get('archive_page_no')}`;
+    const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?id=${encodeURIComponent(uniqueId)}`;
+    window.history.pushState({ path: newUrl }, '', newUrl);
+}
+
+function checkUrlForMarker() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const markerId = urlParams.get('id');
+    if (markerId) {
+        const marker = markers.find(marker => {
+            const uniqueId = `${marker.get('archive_identifier')}-${marker.get('archive_page_no')}`;
+            return uniqueId === markerId;
+        });
+        if (marker) {
+            google.maps.event.trigger(marker, 'click');
+        }
     }
 }
 
+function updateSelectedMarker(marker) {
+    if (selectedMarker) {
+        selectedMarker.setIcon(createMarkerIcon('blue'));
+    }
+    selectedMarker = marker;
+    marker.setIcon(createMarkerIcon('red'));
+}
+
+// Assign initMap to the window object to make it globally accessible
 window.initMap = initMap;
 
+// Initialization
 document.addEventListener('DOMContentLoaded', function() {
+    loadGoogleMapsApi('initMap');
     addMonthCheckboxes();
-    document.querySelectorAll('.month-checkbox').forEach(function(checkbox) {
+    document.querySelectorAll('.month-checkbox').forEach(checkbox => {
         checkbox.addEventListener('change', filterMarkersByMonth);
     });
 });
