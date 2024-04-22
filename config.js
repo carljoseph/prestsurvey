371223b2fee7cae29a0cbc0c73fd9617455218c0
Carl Joseph <carl@carljoseph.com.au>
2024-04-22 10:40:49 +1000

    revert key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const LOCAL_API_KEY = 'AIzaSyColZhkny9WIgMwoSsM6d92XkF3JM0ANtc';
-const DEPLOYED_API_KEY = '';
+const LOCAL_API_KEY = '';
+const DEPLOYED_API_KEY = 'AIzaSyBCM2ItkeZw0o6oOCvpQ5neK2O4eAW1Btk';
 
 const Config = {
     apiKey: window.location.hostname === 'localhost' ? LOCAL_API_KEY : DEPLOYED_API_KEY
