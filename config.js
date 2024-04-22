11a2155d2dfdc9af0e0ef760cfc69a8523964214
Carl Joseph <carl@carljoseph.com.au>
2024-04-22 10:34:15 +1000

    deploy key

---
+++ config.js
@@ -0,0 +1,7 @@
+const DEPLOYED_API_KEY = 'AIzaSyBCM2ItkeZw0o6oOCvpQ5neK2O4eAW1Btk';
+
+const Config = {
+    apiKey: window.location.hostname === 'localhost' ? LOCAL_API_KEY : DEPLOYED_API_KEY
+};
+
+export default Config;
